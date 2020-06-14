3ea73cc02a65bdb32149b20c8ecdeaba1f27c2c5
Lucaster85 <lucasdc87@gmail.com>
2020-06-14 11:17:43 -0300

    creo las rutas de usuarios

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var usersController = require('../controllers/usersController')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', usersController.users)
+
+router.get('/register', usersController.register);
+router.post('/register', usersController.create);
+
+router.get('/login', usersController.login);
+router.post('/login', usersController.processLogin);
+
+router.get('/logout', usersController.logout)
 
 module.exports = router;
