de683db7dea30005823ae92649879cf9f2f6620c
Lucaster85 <lucasdc87@gmail.com>
2020-06-14 12:12:25 -0300

    agrego ruta, controlador y vista para contacto y about

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,14 @@
 module.exports = {
     index(req, res) {
         res.render('index', { title: 'Colola Ceramica' })
+    },
+    contact(req, res) {
+        res.render('contact', {title: 'FORMULARIO DE CONTACTO'})
+    },
+    sendMessege(req, res) {
+        res.redirect('/')
+    },
+    about(req, res) {
+        res.send('SOBRE NOSOTROS')
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,11 @@ var indexController = require('../controllers/indexController')
 /* GET home page. */
 router.get('/', indexController.index);
 
+router.get('/contact', indexController.contact);
+router.post('/contact', indexController.sendMessege);
+
+router.get('/about', indexController.about);
+
+
+
 module.exports = router;
