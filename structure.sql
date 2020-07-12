fe5a73d9a9a524d01f41853b47525673c3199b5e
Lucaster85 <lucasdc87@gmail.com>
2020-07-12 19:31:19 -0300

    correccion de errores en diagrama y query de base de datos

---
+++ structure.sql
@@ -0,0 +1,138 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema cololaCeramica
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema cololaCeramica
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `cololaCeramica` DEFAULT CHARACTER SET utf8 ;
+USE `cololaCeramica` ;
+
+-- -----------------------------------------------------
+-- Table `cololaCeramica`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cololaCeramica`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `avatar` VARCHAR(255) NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT NULL,
+  `delete` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`));
+
+
+-- -----------------------------------------------------
+-- Table `cololaCeramica`.`categories`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cololaCeramica`.`categories` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `image` VARCHAR(255) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL,
+  `vajilla` TINYINT NOT NULL DEFAULT 0,
+  `decoracion` TINYINT NOT NULL DEFAULT 0,
+  `delete` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`));
+
+
+-- -----------------------------------------------------
+-- Table `cololaCeramica`.`products`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cololaCeramica`.`products` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `price` INT NOT NULL,
+  `image1` VARCHAR(255) NOT NULL,
+  `image2` VARCHAR(255) NULL,
+  `image3` VARCHAR(255) NULL,
+  `description` VARCHAR(255) NOT NULL,
+  `outstanding` TINYINT NULL DEFAULT 0,
+  `category_id` INT NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT NULL,
+  `delete` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `category_id`
+    FOREIGN KEY (`id`)
+    REFERENCES `cololaCeramica`.`categories` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table `cololaCeramica`.`carts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cololaCeramica`.`carts` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `status` TINYINT NOT NULL DEFAULT 1,
+  `total` INT NOT NULL,
+  `user_cart_id` INT NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT NULL,
+  `delete` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `user_cart_id`
+    FOREIGN KEY (`id`)
+    REFERENCES `cololaCeramica`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table `cololaCeramica`.`cart_products`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cololaCeramica`.`cart_products` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `product_id` INT NOT NULL,
+  `cart_id` INT NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT NULL,
+  `delete` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `product_id`
+    FOREIGN KEY (`id`)
+    REFERENCES `cololaCeramica`.`products` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `cart_id`
+    FOREIGN KEY (`id`)
+    REFERENCES `cololaCeramica`.`carts` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table `cololaCeramica`.`addresses`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cololaCeramica`.`addresses` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `city` VARCHAR(100) NOT NULL,
+  `postcode` INT NOT NULL,
+  `street` VARCHAR(100) NOT NULL,
+  `number` INT NOT NULL,
+  `flour` VARCHAR(45) NULL DEFAULT NULL,
+  `apartment` VARCHAR(45) NULL DEFAULT NULL,
+  `user_address_id` INT NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT NULL,
+  `delete` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `user_address_id`
+    FOREIGN KEY (`id`)
+    REFERENCES `cololaCeramica`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
