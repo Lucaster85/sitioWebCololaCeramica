2bdf330540c0083f49cc88e8fb22ddbeb316d1eb
Lucaster85 <lucasdc87@gmail.com>
2020-07-12 18:42:50 -0300

    Creacion del modelo de la tabla users

---
+++ database/models/User.js
@@ -0,0 +1,58 @@
+module.exports = (sequelize, dataTypes) => {
+    
+    const alias = 'User';
+
+    const cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        name: {
+            type: dataTypes.STRING(100),
+            allowNull: false
+        },
+        email: {
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+        avatar: {
+            type: dataTypes.STRING,
+            defaultValue: null
+        },
+        delete: {
+            type: dataTypes.BOOLEAN,
+            defaultValue: 0
+        }
+    }
+
+    const config = {
+        tableName: 'users',
+        underscored: true,
+        timestamps: true,
+        createdAt: 'created_at',
+        updetadAt: 'updated_at'
+    }
+
+    const User = sequelize.define(alias, cols, config);
+
+    User.associate = (models) => {
+        User.hasMany(models.Address, {
+            as: 'addresses',
+            foreignKey: 'user_address_id'
+        })
+    }
+    User.associate = (models) => {
+        User.hasMany(models.Cart, {
+            as: 'carts',
+            foreignKey: 'user_id'
+        })
+    }
+
+    return User;
+}
