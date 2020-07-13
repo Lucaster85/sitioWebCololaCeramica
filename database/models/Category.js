b999d214805d1a0dc9d83e234a5b75a309a023f2
Lucaster85 <lucasdc87@gmail.com>
2020-07-13 13:26:28 -0300

    creacion del modelo de la tabla categories

---
+++ database/models/Category.js
@@ -0,0 +1,54 @@
+module.exports = (sequelize, dataTypes) => {
+
+    const alias = 'Category';
+
+    const cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        title: {
+            type: dataTypes.STRING,
+            allowNull: false,
+        },
+        image: {
+            type: dataTypes.STRING,
+            allowNull: false,
+        },
+        vajilla: {
+            type: dataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: 0
+        },
+        decoracion: {
+            type: dataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: 0
+        },
+        delete: {
+            type: dataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: 0
+        }
+    };
+
+    const config = {
+        tableName: 'categories',
+        underscored: true,
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
+
+    const Category = sequelize.define(alias, cols, config);
+
+    Category.associate = models => {
+        Category.hasMany(models.products, {
+            as: 'products',
+        })
+    };
+
+    return Category;
+}
