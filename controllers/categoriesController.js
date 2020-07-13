165f040ba43a9ce6f56a24b7aef362403a94e745
Lucaster85 <lucasdc87@gmail.com>
2020-07-13 18:05:16 -0300

    creacion controlador de la ruta categories

---
+++ controllers/categoriesController.js
@@ -0,0 +1,19 @@
+const db = require('../database/models');
+
+module.exports = {
+    list(req, res) {
+        
+    },
+    detail(req, res) {
+
+    },
+    store(req, res) {
+
+    },
+    update(req, res) {
+
+    },
+    delete(req, res) {
+
+    }
+};
