1250d3d7dea910494366db807a3e2700eb2e7830
Lucaster85 <lucasdc87@gmail.com>
2020-07-12 22:44:23 -0300

    creacion del modelo de la tabla carts

---
+++ database/models/Cart.js
@@ -0,0 +1,60 @@
+module.exports = (sequelize, dataTypes) => {
+
+    const alias = 'Cart';
+
+    const cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        status: {
+            type: dataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: 1
+        },
+        total: {
+            type: dataTypes.INTEGER,
+            allowNull: false
+        },
+        user_cart_id: {
+            type: dataTypes.INTEGER,
+            allowNull: false,
+            foreignKey: true,
+            references: {
+                model: 'User',
+                key: 'id'
+            }
+        },
+        delete: {
+            type: dataTypes.BOOLEAN,
+            defaultValue: 0
+        }
+    }
+
+    const config = {
+        tableName: 'carts',
+        underscored: true,
+        timestamp: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    } 
+
+    const Cart = sequelize.define(alias, cols, config);
+
+    Cart.associate = models => {
+        Cart.belongsTo(models.User, {
+            as: 'user',
+            foreignKey: 'user_cart_id'
+        });
+        Cart.belongsToMany(models.Products, {
+            as: 'products',
+            through: 'cart_products',
+            foreignKey: 'product_id',
+            otherKey: 'cart_id'
+        })
+    }
+
+    return Cart;
+}
