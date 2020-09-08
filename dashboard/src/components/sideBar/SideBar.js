99624dbc93944ea75c4b0f812acad31f09bc7844
Lucaster85 <lucasdc87@gmail.com>
2020-09-08 11:28:40 -0300

    empiezo a trabajar en dashboard

---
+++ dashboard/src/components/sideBar/SideBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SideBAr extends Component {
+    render() {
+        return(
+            <div className="sideBar">
+                <h1>BARRA LATERAL</h1>
+            </div>
+        )
+    }
+}
+
+export default SideBAr;
